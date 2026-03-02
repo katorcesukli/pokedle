d417e12847f33e9918ccc90b530d67bdc88fee06
restrellado <rianestrellado3@gmail.com>
2026-03-02 13:54:33 +0800

    test

---
+++ src/main/java/org/personal/pokedle/PokedleMavenApplication.java
@@ -0,0 +1,13 @@
+package org.personal.pokedle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedleMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedleMavenApplication.class, args);
+    }
+
+}
